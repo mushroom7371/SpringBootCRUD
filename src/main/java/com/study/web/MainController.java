281af078f07ded1c42ee3d24453287ffac421eb3
juna <wnsghk6670@naver.com>
2022-03-02 17:59:21 +0900

    main jsp 추가

---
+++ src/main/java/com/study/web/MainController.java
@@ -0,0 +1,26 @@
+package com.study.web;
+
+////컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
+//@RestController
+//public class HelloController {
+//
+//    //HTTP Method인 Get의 요청을 받 을 수 있는 API를 만듦
+//    //이전의 @RequestMapping(method = RequestMethod.GET)이랑 비슷
+//    @GetMapping("/hello")
+//    public String Hello(){
+//        return "Hello";
+//    }
+//
+//}
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "main")
+    public String main(){
+        return "main";
+    }
+}
